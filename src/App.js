6993bdd0d59ca2bb207d6f25791ee4199468ac35
Toyin96 <onagoruwam@gmail.com>
2021-04-01 17:08:26 +0100

    implemented register page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Shop from './pages/shop';
 import ProductDetail from './pages/detail';
+import RegisterPage from './pages/register';
 function App() {
   return (
     <>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/shop' component={Shop} />
+        <Route exact path = '/register' component={RegisterPage}/>
         <Route exact path ='/:productId' component={ProductDetail} />
+
       </Switch>
     </>
   );
