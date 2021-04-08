79b139e66657ef2af371e5308d84d89b8d0a2e25
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-04-08 16:49:41 +0100

    login refactor

--- src/pages/register.js
+++ src/pages/register.js
@@ -9,38 +9,46 @@ const RegisterPage = ()=> {
         <>
             <Header/>
             <PageHeader/>
-            <div className="overall">
-                <div className="image">
-                    <div >
-                        <h2><b>New to our website?</b></h2>
-                        <p>There are advances being made in science and technology</p>
-                        <p>everyday, and a good example of this is the</p>
-                        <button>CREATE AN ACCOUNT</button>
-                    </div>
-                </div>
-                <div className="register">
-                    <form>
-                    <h3><b>Register an account</b></h3>
-                    <input type="text" placeholder="Enter Email"required></input>
-                    <input type="password" placeholder="Enter Password"required></input>
-                    <input type="password" placeholder="Repeat Password"required></input>
-                    
-                    <div className="checking">
-                        <div className="box">
-                            <input type="checkbox" id="check" required></input>
-                        </div>
-                        <div className="label">
-                            <label for="check">By creating an account you agree to our Terms & Privacy</label>
+            <div className="container-register">
+                <div className='register-form-container'>
+                    <div className="image">
+                        <div className='image-text'>
+                            <h2>New to our website?</h2>
+                            <p>There are advances being made in science and technology
+                             everyday, and a good example of this is the ....
+                            </p>
+                            <button>CREATE AN ACCOUNT</button>
                         </div>
                     </div>
-                    
-                    <button type="submit">Register</button>
-
+                    <div className="register">
+                        <form  className='register-form'>
+                            <h3>Register an account</h3>
+                            <div className='form-control-double'>
+                                <input type="text" placeholder="Firstname" required/>
+                                <input type="text" placeholder="Lastname"required/>
+                            </div>
+                            <div className='form-control-full'>
+                                <input type='email' placeholder='email' required />
+                            </div>
+                            <div className='form-control-double'>
+                                <input type="password" placeholder="Enter Password"required/>
+                                <input type="password" placeholder="Repeat Password"required/>
+                            </div>
+                            <div className="checking">
+                                <div className="box">
+                                    <input type="checkbox" id="check" required></input>
+                                </div>
+                            <div className="label">
+                                <label for="check">By creating an account you agree to our Terms & Privacy</label>
+                            </div>
+                        </div>
+                        <button type="submit">Register</button>
                     <div class="signin">
                         <p>Already have an account? <a href="/login">Sign in</a>.</p>
                     </div>
 
                     </form>
+                </div>
                 </div>
             </div>
             <Footer/>
